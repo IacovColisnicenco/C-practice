acabb1aa5f359df7ab2feca89b64d9b600626e01
Yakov <jacobwebm@gmail.com>
2021-02-08 20:18:25 +0200

    zlatopoliskii - 1400 zada4, task_2.21

--- main.c
+++ main.c
@@ -7,7 +7,17 @@ Task name:
 */
 int main() {
     //Initializing Variables
+    float  e, f, g , h;
+    float a,b,c;
 
+    printf("enter e,f,g,h - values: ");
+    scanf("%f%f%f%f",&e,&f,&g,&h);
+
+    a = (e + f/2)/3;
+    b = abs(h*h - g);
+    c = (g*g - 2*g*h + h*h) - 3*sin(e);
+
+    printf("func. values a= %f, b =%f, c = %f\n",a,b,c); 
 
     return 0;
 }
