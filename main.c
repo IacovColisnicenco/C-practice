f168da53231030fbdd15866ed49420b4f0d3bc20
Yakov <jacobwebm@gmail.com>
2021-02-09 17:51:27 +0200

    zlatopoliskii - 1400 zada4, task_2.22

--- main.c
+++ main.c
@@ -3,11 +3,20 @@
 #include <math.h>
 /*
 Version: 1.0
-Task name:
+Task name: Zlatopolskii 1400 zada4 2.22
 */
 int main() {
     //Initializing Variables
+    float a , b, A, G;
 
+    printf("enter a, b numbers: ");
+    scanf("%f%f",&a,&b);
+    a = abs(a);
+    b = abs(b);
+    printf("mod a:%f, mod b:%f\n",a,b);
+    A = (a+b)/2;
+    G = sqrt(a*b);
+    printf("Sred. arifm.: %f, Sred. geom.: %f",A,G);
 
     return 0;
 }
