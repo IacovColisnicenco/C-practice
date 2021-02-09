724706201404832e97a5cc2c4c5d8bcbd06fc301
Yakov <jacobwebm@gmail.com>
2021-02-09 22:43:55 +0200

    Zlatopolskii 1400 zada4 task_2.23

--- main.c
+++ main.c
@@ -7,6 +7,16 @@ Task name:
 */
 int main() {
     //Initializing Variables
+    float P, a, b, d;
+
+    printf("Find the perimeter of a rectangle, enter a, b: ");
+    scanf("%f%f",&a,&b);
+
+    P = 2*(a + b);
+    d = sqrt((a*a) + (b*b));
+
+    printf("Perimeter of a rectangle P=%f\n", P);
+    printf("Diagonal length of a rectangle d=%f",d);   
 
 
     return 0;
