85f51333bef5563a2a6e7ea6d0623aced0b11642
Yakov <jacobwebm@gmail.com>
2021-02-10 15:52:11 +0200

    task_2.24, three-variable solution

--- main.c
+++ main.c
@@ -3,10 +3,25 @@
 #include <math.h>
 /*
 Version: 1.0
-Task name:
+Task name: 2.24 1400 zada4, Zlatopolskii
 */
 int main() {
-    //Initializing Variables
+    //Initializing Variables, three-variable solution
+    int a,b,c;
+
+printf("Enter two numbers: ");
+scanf("%d%d",&a,&b);
+c = a + b;
+printf("Sum is: %d",c );
+c = abs(a - b);
+printf("\nDifference is: %d",c );
+c = a*b;
+printf("\nMultiplication is: %d",c );
+//divide
+c = a/b;
+printf("\nDivide: %d",c );
+
+
 
 
     return 0;
