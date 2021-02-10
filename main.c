1e7728c36dc412f8590d12ad565babc85486f840
Yakov <jacobwebm@gmail.com>
2021-02-10 16:05:51 +0200

    task_2.24 two-variable solution

--- main.c
+++ main.c
@@ -4,22 +4,31 @@
 /*
 Version: 1.0
 Task name: 2.24 1400 zada4, Zlatopolskii
+Эту задачу можно решить с двумя переменными, но возрастает число команд ввода/выода
 */
 int main() {
-    //Initializing Variables, three-variable solution
-    int a,b,c;
+    //Initializing Variables, two-variable solution
+    int a,b;
 
 printf("Enter two numbers: ");
 scanf("%d%d",&a,&b);
-c = a + b;
-printf("Sum is: %d",c );
-c = abs(a - b);
-printf("\nDifference is: %d",c );
-c = a*b;
-printf("\nMultiplication is: %d",c );
+a = a + b;
+printf("Sum is: %d",a );
+
+printf("Enter two numbers: ");
+scanf("%d%d",&a,&b);
+a = abs(a - b);
+printf("\nDifference is: %d",a );
+
+printf("Enter two numbers: ");
+scanf("%d%d",&a,&b);
+a = a*b;
+printf("\nMultiplication is: %d",a );
 //divide
-c = a/b;
-printf("\nDivide: %d",c );
+printf("Enter two numbers: ");
+scanf("%d%d",&a,&b);
+a = a/b;
+printf("\nDivide: %d",a );
 
 
 
