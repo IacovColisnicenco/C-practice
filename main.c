856cf5c20b87ea5d486945456a187c85c433665e
Yakov <jacobwebm@gmail.com>
2021-02-11 14:34:51 +0200

    task_2.25 1400 zada4, Zlatopoliskii

--- main.c
+++ main.c
@@ -3,11 +3,16 @@
 #include <math.h>
 /*
 Version: 1.1
-Task name:
+Task 2.25: Даны длины сторон прямоугольного параллелепипеда.
+Найти его объем и  площадь боковой поверхности
 */
 int main() {
-    //Initializing Variables
+  int V, S_bok, a, b, c; //Initializing Variables
 
-
-    return 0;
+  printf("Enter a, b, c: ");
+  scanf("%d%d%d",&a,&b,&c);
+    V = a*b*c;
+    S_bok = 2*((a*b) + (a*c));
+  printf("V = %d, S_bok= %d", V,S_bok);
+  return 0;
 }
