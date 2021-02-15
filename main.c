1140db7d407e8b091161dca2724fce3d777a8b0a
Yakov <jacobwebm@gmail.com>
2021-02-15 19:48:43 +0200

    add task_2.28

--- main.c
+++ main.c
@@ -3,10 +3,22 @@
 #include <math.h>
 /*
 Version: 1.1
-Task name:
+Task name: 2.28.	 Даны основания равнобедренной трапеции и  угол при
+большем основании. Найти площадь трапеции.
 */
 int main() {
     //Initializing Variables
+    float a, b, angle, angle_rad, S, h;
+
+    puts("Please enter trapezoid base, a(min base), b(max base): ");
+    scanf("%f%f",&a,&b);
+    puts("Please enter angle (in radians) exemple value - 30, 45 etc.: ");
+    scanf("%f", &angle);
+    angle_rad = (angle*3.14)/180; //переводим градусы в радианы т.к функция tanf() принимает аргумент в радианах
+    h = 2*tanf( angle_rad );
+    S = ((a + b)/2)*h;
+
+    printf("Trapezoid area %f", S);
 
 
     return 0;
