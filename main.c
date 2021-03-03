327c7c423bf73dbb3cdf419da33e063008c0314d
Yakov <jacobwebm@gmail.com>
2021-03-03 12:06:55 +0200

    update main.c

--- main.c
+++ main.c
@@ -3,22 +3,13 @@
 #include <math.h>
 /*
 Version: 1.1
-Task name: 2.28.	 Даны основания равнобедренной трапеции и  угол при
-большем основании. Найти площадь трапеции.
+Task name: 2.29.	 Треугольник задан координатами своих вершин.
+Найти периметр и  площадь треугольника.
+
 */
 int main() {
     //Initializing Variables
-    float a, b, angle, angle_rad, S, h;
-
-    puts("Please enterq trapezoid base, a(min base), b(max base): ");
-    scanf("%f%f",&a,&b);
-    puts("Please enter angle (in degree) exemple value - 30, 45 etc.: ");
-    scanf("%f", &angle);
-    angle_rad = (angle*3.14)/180; //переводим градусы в радианы т.к функция tanf() принимает аргумент в радианах
-    h = 2*tanf( angle_rad );
-    S = ((a + b)/2)*h;
-
-    printf("Trapezoid area %f", S);
+  
 
 
     return 0;
